dee84e78a14a1a6132efbef87b728a57f271dcb3
zhouwanqing <zhouwanqing@chezhibao.com>
2019-09-21 18:08:38 +0800

    添加打包

--- src/main/java/com/olency/docker/DockerApplication.java
+++ src/main/java/com/olency/docker/DockerApplication.java
@@ -3,6 +3,7 @@ package com.olency.docker;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DockerApplication {
@@ -11,4 +12,8 @@ public class DockerApplication {
         SpringApplication.run(DockerApplication.class, args);
     }
 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(DockerApplication.class);
+    }
 }
